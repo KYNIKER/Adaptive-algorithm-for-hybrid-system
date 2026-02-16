8700b68991abf73819d6cdb9833d50072d37f9e3
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2026-02-16 10:24:04 +0100

    Base

---
+++ ReACTed.jl
@@ -0,0 +1,205 @@
+using LazySets, LinearAlgebra
+include("Discretize.jl")
+
+function ReACT(A, B, initialTimeStep, interval, X0::Zonotope{N,Vector{N},Matrix{N}}, U::Zonotope, constraint, Digits::Integer, STRATEGY::Integer, alg::ReachabilityAnalysis.Exponentiation.AbstractExpAlg=ReachabilityAnalysis.Exponentiation.BaseExp, maxOrder::Int=5, reduceOrder::Int=5) where {N}
+
+    m = initialTimeStep / 2^(ceil(Integer, log2(initialTimeStep)) + ceil(Integer, -log2(10.0^(-Digits))) - 1)   #Calculate the smallest number larger than 10^-Digits obtained by repeatedly dividing initialTimeStep by 2.
+    changedTimeStep = true
+    elems = (ceil(Integer, log2(initialTimeStep)) + ceil(Integer, -log2(10.0^(-Digits))) - 1)
+    phiDict = Dict{Float64,Matrix{Float64}}()
+    sizehint!(phiDict, elems)
+    discritezationDict = Dict{Float64,Zonotope{N,Vector{N},Matrix{N}}}()
+    sizehint!(discritezationDict, elems)
+    inputDiscritezationDict = Dict{Float64,Zonotope{N,Vector{N},Matrix{N}}}()
+    sizehint!(inputDiscritezationDict, elems)
+
+    constraintProjVectors = map(x -> x.a, constraint)
+    constraintProjBounds = ρ.(constraintProjVectors, constraint)
+
+    discritezationDict, inputDiscritezationDict, phiDict = ReACTDiscretize(A, B, X0, U, m, initialTimeStep, alg, maxOrder, reduceOrder)
+
+    time::Float64 = minimum(interval)
+    endtime::Float64 = maximum(interval)
+
+    currentTimeStep = copy(initialTimeStep)
+
+    attemptsRecorder = Integer[]
+
+    V::Zonotope{N,Vector{N},Matrix{N}} = copy(inputDiscritezationDict[initialTimeStep])
+    Sρ = zeros(Float64, length(constraint))
+    newR::Zonotope{N,Vector{N},Matrix{N}} = discritezationDict[initialTimeStep]
+    i = 1
+
+
+    Φ::Matrix{Float64} = diagm(ones(Float64, size(A, 2)))
+    tempM = similar(Φ)
+    ϕt = similar(Φ)
+    newRR = copy(newR)
+
+
+    while time < endtime
+
+        attempts = 1
+        approveFlag = false
+
+        while !approveFlag
+            if currentTimeStep < m
+                return false
+            end
+
+            if changedTimeStep
+                newR = discritezationDict[currentTimeStep]
+                V = copy(inputDiscritezationDict[currentTimeStep])
+                ϕt = phiDict[currentTimeStep]
+                newRR = linear_map(Φ, newR)
+                V = linear_map(Φ, V)
+            else
+                newRR = linear_map(ϕt, newRR)
+                V = linear_map(ϕt, V)
+            end
+
+            changedTimeStep = false
+            hom = map(x -> ρ(x, newRR), constraintProjVectors)
+            inhom = map(x -> ρ(x, V), constraintProjVectors)
+
+            if reduce(&, <=(Sρ + hom + inhom, constraintProjBounds))
+                approveFlag = true
+                Sρ += inhom
+                mul!(tempM, Φ, ϕt)
+                copy!(Φ, tempM)
+            else
+                newR = copy(newR)
+                currentTimeStep = currentTimeStep / 2
+                changedTimeStep = true
+                attempts = attempts + 1
+            end
+        end
+
+
+        push!(attemptsRecorder, attempts)
+        i = i + 1
+        time = time + currentTimeStep
+
+        # Reset / apply strategy
+        # Only do this if the current timestep is less than the initial
+        if STRATEGY == 0
+            # Only reduce
+        elseif STRATEGY == 1
+            # always try double
+            if currentTimeStep < initialTimeStep
+                currentTimeStep = currentTimeStep * 2
+                changedTimeStep = true
+            end
+        elseif STRATEGY == 2
+            # If attemptsrecorder past 4 are successes, double timestep
+            if currentTimeStep < initialTimeStep
+                lowest = min(4, i - 1)
+                window = @view attemptsRecorder[i-lowest:i-1]
+                if all(window .== 1)
+                    currentTimeStep = currentTimeStep * 2
+                    changedTimeStep = true
+                end
+            end
+        end
+    end
+
+    return true
+end
+
+function ReACT(A, B, initialTimeStep, interval, X0::Zonotope{N,Vector{N},Matrix{N}}, U::Nothing, constraint, Digits::Integer, STRATEGY::Integer, alg::ReachabilityAnalysis.Exponentiation.AbstractExpAlg=ReachabilityAnalysis.Exponentiation.BaseExp, maxOrder::Int=5, reduceOrder::Int=5) where {N}
+    XG = copy(genmat(X0))
+    XDim, _ = size(XG)
+    m = initialTimeStep / 2^(ceil(Integer, log2(initialTimeStep)) + ceil(Integer, -log2(10.0^(-Digits))) - 1)   #Calculate the smallest number larger than 10^-Digits obtained by repeatedly dividing initialTimeStep by 2.
+    changedTimeStep = true
+
+    elems = (ceil(Integer, log2(initialTimeStep)) + ceil(Integer, -log2(10.0^(-Digits))) - 1)
+    phiDict = Dict{Float64,Matrix{Float64}}()
+    sizehint!(phiDict, elems)
+    discritezationDict = Dict{Float64,Zonotope{N,Vector{N},Matrix{N}}}()
+    sizehint!(discritezationDict, elems)
+
+    discritezationDict, _, phiDict = ReACTDiscretize(A, B, X0, U, m, initialTimeStep, alg, maxOrder, reduceOrder)
+
+    constraintProjVectors = map(x -> x.a, constraint)
+    constraintProjBounds = ρ.(constraintProjVectors, constraint)
+
+    time::Float64 = minimum(interval)
+    endtime::Float64 = maximum(interval)
+
+    currentTimeStep = copy(initialTimeStep)
+
+    attemptsRecorder = Integer[]
+
+    newR::Zonotope{N,Vector{N},Matrix{N}} = discritezationDict[initialTimeStep]
+    i = 1
+
+
+    Φ::Matrix{Float64} = diagm(ones(Float64, size(A, 2)))
+    tempM = similar(Φ)
+    ϕt = similar(Φ)
+    newRR = copy(newR)
+    initialϕ = phiDict[initialTimeStep]
+
+    while time < endtime
+
+        attempts = 1
+        approveFlag = false
+
+        while !approveFlag
+            if currentTimeStep < m
+                println(time)
+                return false
+            end
+
+            if changedTimeStep
+                newR = discritezationDict[currentTimeStep]
+                ϕt = phiDict[currentTimeStep]
+                newRR = linear_map(Φ, newR)
+            else
+                newRR = linear_map(ϕt, newRR)
+            end
+
+            changedTimeStep = false
+
+            hom = map(x -> ρ(x, newRR), constraintProjVectors)
+            if reduce(&, <=(hom, constraintProjBounds))
+                approveFlag = true
+                mul!(tempM, Φ, ϕt)
+                copy!(Φ, tempM)
+            else
+                newR = copy(newR)
+                currentTimeStep = currentTimeStep / 2
+                changedTimeStep = true
+                attempts = attempts + 1
+            end
+        end
+
+        push!(attemptsRecorder, attempts)
+        i = i + 1
+        time = time + currentTimeStep
+
+        # Reset / apply strategy
+        # Only do this if the current timestep is less than the initial
+        if STRATEGY == 0
+            # Only reduce
+        elseif STRATEGY == 1
+            # always try double
+            if currentTimeStep < initialTimeStep
+                currentTimeStep = currentTimeStep * 2
+                changedTimeStep = true
+            end
+        elseif STRATEGY == 2
+            # If attemptsrecorder past 4 are successes, double timestep
+            if currentTimeStep < initialTimeStep
+                lowest = min(4, i - 1)
+                window = @view attemptsRecorder[i-lowest:i-1]
+                if all(window .== 1)
+                    currentTimeStep = currentTimeStep * 2
+                    changedTimeStep = true
+                end
+            end
+        end
+    end
+
+    return true
+end

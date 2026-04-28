a34c5af09731c2e3a9aaf831f774339708d2f129
Grace-Melchiors <melchiorsgrace@gmail.com>
2026-04-28 10:03:38 +0200

    feat! TestSuite!

---
+++ testsuite.jl
@@ -0,0 +1,74 @@
+using LazySets, LinearAlgebra, BenchmarkTools, CSV, DataFrames, Expokit, ReachabilityAnalysis
+
+include("Utilities.jl")
+include("ReACTed.jl")
+include("models/gearbox.jl")
+include("models/platoon.jl")
+include("models/bouncingBall.jl")
+include("models/powerTrain.jl")
+include("models/spacecraft.jl")
+
+const RUN_FIXED = true
+const RUN_ADAPTIVE = false
+
+function RunAdaptive(name, δ⁻, δ⁺, load_func)
+    sys, initialState, X0, T = load_func()
+    n = length(X0.center)
+    LazySets.load_expokit()
+    println("Running benchmark for: ", name)
+    BenchmarkTools.DEFAULT_PARAMETERS.samples = 10
+
+    b = @benchmarkable _ = ReACTed($sys, $initialState, [0., $T], $X0, $Zonotope(zeros(Float64, $n), $zeros(Float64, $n, 1)), $sys.globalConstraints, $δ⁻, $δ⁺ , ReachabilityAnalysis.Exponentiation.BaseExp, 5, 5, false)
+
+    y = run(b; verbose=true)
+    println("Run completed.")
+
+    # Convert time to seconds from nanoseconds
+    timeList = []
+    for timeVal in y.times
+        push!(timeList, timeVal / 1e9)
+    end
+    # Write to csv file
+    df = DataFrame(δ⁺=δ⁺, δ⁻=δ⁻, avgTime=mean(timeList), medianTime=median(timeList), memory=y.memory, allocs=y.allocs)
+
+    filename = "results/ReACT_" * name * "Results" * ".csv"
+    if isfile(filename)# Check if file exists
+        open(filename, "a") do File
+            CSV.write(File, df, delim=";", append=true)
+        end
+    else
+        open(filename, "w") do File
+            CSV.write(File, df, delim=";", writeheader=true)
+        end
+    end
+
+    println("Finished writing to file")
+end
+
+function RunFixed(name, δ, load_func)
+    RunAdaptive(name, δ, δ, load_func)
+end
+
+
+names = ["bouncingBall", "gearbox", "platoon", "powerTrain", "spaceCraft"]
+minδ = [0.05, 0.008, 0.03, 0.002, 0.04]
+loadFunctions = [loadBouncingBall, loadGearBox, loadPlatoon, loadPowertrain, () -> loadSpacecraft(abort_time=120.)]
+
+# Long Versions
+# names = ["bouncingBall", "gearbox-01", "gearbox-02", "platoon", "powerTrain", "spaceCraft-0", "spaceCraft-120", "spaceCraft-240"]
+# minδ = [0.05, 0.008, 0.008, 0.03, 0.002, 0.04, 0.04, 0.01]
+# funcs = [loadBouncingBall, loadGearBox, () -> loadGearBox(2), loadPlatoon, loadPowertrain, loadSpacecraft, ()-> loadSpacecraft(abort_time=120), ()-> loadSpacecraft(abort_time=240)]
+
+
+if RUN_FIXED
+    for (name, δ, loadFunction) in zip(names, minδ, loadFunctions)
+        RunFixed(name * "_Fixed", δ, loadFunction)
+    end
+end
+
+if RUN_ADAPTIVE
+    for (name, δ⁻, loadFunction) in zip(names, minδ, loadFunctions)
+        δ⁺ = δ⁻ * 2^5 
+        RunAdaptive(name * "_Adaptive", δ⁻, δ⁺, loadFunction)
+    end
+end

2df37ff166f94182635444d73d9dd8ac4de38fac
Mikkel Bjørn <mikkel.bjoern@hotmail.com>
2026-05-15 13:42:17 +0200

    Platoon added

--- JuliaReach/benchmark.jl
+++ JuliaReach/benchmark.jl
@@ -4,21 +4,23 @@ include("embrake/embrake.jl")
 include("embrake/embrake_benchmark.jl")
 include("gearbox/gearbox.jl")
 include("gearbox/gearbox_benchmark.jl")
-#include("platoon/platoon_benchmark.jl")
+include("platoon/platoon.jl")
+include("platoon/platoon_benchmark.jl")
 #include("spacecraft/spacecraft_benchmark.jl")
 
 # template directions
 
 const extdirs = template_directions(6)
+const dirs = CustomDirections(collect(OctDirections{Float64,Vector{Float64}}(10)))
 
 cases = [
     #"BRKDC01",
     #"BRKDC01-discrete",
-    "GRBX01-MES01",
-    "GRBX01-MES01-discrete",
-    #="PLAD01-BND30",
+    #"GRBX01-MES01",
+    #"GRBX01-MES01-discrete",
+    "PLAD01-BND30",
     "PLAD01-BND30-discrete",
-    "SRA01",
+    #="SRA01",
     "SRA01-discrete"=#
 ]
 
@@ -26,10 +28,10 @@ algCheckDict = Dict(
     "BRKDC01" =>  GLGM06(δ=2e-7, max_order=1, static=true, dim=4, ngens=4, approx_model=Forward()),
     "BRKDC01-discrete" => GLGM06(δ=1e-8, max_order=1, static=true, dim=4, ngens=4, approx_model=NoBloating()),
     "GRBX01-MES01" => LGG09(δ=0.0008, template=extdirs, approx_model=Forward()),
-    "GRBX01-MES01-discrete" => LGG09(δ=0.0001, template=extdirs, cache=true, approx_model=NoBloating())
-    #="PLAD01-BND30" => LGG09(δ=0.03, template=dirs, approx_model=Forward(setops=dirs)),
-    "PLAD01-BND30-discrete" => LGG09(δ=δ_discrete, template=dirs, approx_model=NoBloating()),
-    "SRA01" => BOX(δ=0.04),
+    "GRBX01-MES01-discrete" => LGG09(δ=0.0001, template=extdirs, cache=true, approx_model=NoBloating()),
+    "PLAD01-BND30" => LGG09(δ=0.03, template=dirs, approx_model=Forward(setops=dirs)),
+    "PLAD01-BND30-discrete" => LGG09(δ=0.1, template=dirs, approx_model=NoBloating())
+    #="SRA01" => BOX(δ=0.04),
     "SRA01-discrete" => BOX(δ=0.1, approx_model=NoBloating())=#
 )
 
@@ -52,17 +54,8 @@ function JuliaReachTest(name)
     GC.gc()
 
     alg = algCheckDict[name]
-    modelname = caseCheckDict[name]
 
-    if modelname == "embrake" 
-        sol, df = runEMBrake(alg, name)
-    elseif modelname == "gearbox"
-        sol, df = runGearbox(alg, name)
-    elseif modelname == "platoon"
-
-    elseif modelname == "spacecraft"
-
-    end
+    sol, df = run(alg, name)
 
     # Get amonut of steps taken
     # println(length(sol))
@@ -78,9 +71,22 @@ function JuliaReachTest(name)
             CSV.write(File, df, delim=";", writeheader=true)
         end
     end
-    
 end
 
+function run(alg, case)
+    modelname = caseCheckDict[case]
+    if modelname == "embrake" 
+        sol, df = runEMBrake(alg, case)
+    elseif modelname == "gearbox"
+        sol, df = runGearbox(alg, case)
+    elseif modelname == "platoon"
+        sol, df = runPlatoon(alg, case)
+    elseif modelname == "spacecraft"
+        sol, df = runSpacecraft(alg, case)
+    end
+
+    return sol, df
+end
 
 function runEMBrake(alg, case)
     prob_no_pv_no_jit = embrake_no_pv(ζ=0.)
@@ -102,7 +108,13 @@ function runGearbox(alg, case)
 end
 
 function runPlatoon(alg, case)
-    
+    prob_PLAD01_BND30 = platoon(; deterministic_switching=true)
+    cmethod = LazyClustering(1)
+    dirs = CustomDirections(collect(OctDirections{Float64,Vector{Float64}}(10)))
+    imethod = TemplateHullIntersection(dirs)
+    dmin = -30.0
+
+    sol, df = analyze_platoon(prob_PLAD01_BND30, alg, cmethod, imethod, dmin, case, true)
 
     return sol, df
 end

599e8093241da4f325027e32427deeb8405f36dc
Grace-Melchiors <melchiorsgrace@gmail.com>
2026-06-08 17:15:47 +0200

    update manifest

--- setupManifest.jl
+++ setupManifest.jl
@@ -3,6 +3,6 @@ Pkg.activate(".")
 
 # This creates a manifest, with the following packages
 
-libraries = ["Plots", "LazySets", "LinearAlgebra", "BenchmarkTools", "CSV", "DataFrames", "Expokit", "SparseArrays", "ReachabilityAnalysis", "Polyhedra", "Optim", "CDDLib", "LaTeXStrings"]
+libraries = ["Plots", "LazySets", "LinearAlgebra", "BenchmarkTools", "CSV", "DataFrames", "Expokit", "SparseArrays", "ReachabilityAnalysis", "Polyhedra", "Optim", "CDDLib", "LaTeXStrings", "ReachabilityBase"]
 
 Pkg.add(libraries)

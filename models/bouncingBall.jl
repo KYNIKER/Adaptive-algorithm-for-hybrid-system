7afbd6e9625bbcb0baafafc3548281ebbd25ae85
Daniel Hilo Hansen <danielhilob@gmail.com>
2026-09-21 11:50:43 +0200

    Feat: new bouncing ball model with inputs

--- models/bouncingBall.jl
+++ models/bouncingBall.jl
@@ -36,8 +36,8 @@ function loadBouncingBall()
 end
 
 function loadBouncingBallShielded()
-    A = [0.0 1.0 0.0; 0.0 0.0 -(9.82/ℯ); 0. 0. 0.]
-    S = Hyperrectangle([7.5, 0.0, 1.0], [8.5, 15.0, 0]) # p: [-1, 16], v: [-15, 15], g: [1,1]
+    A = [0.0 1.0 0.0; 0.0 0.0 -9.82; 0. 0. 0.]
+    S = Hyperrectangle([8.5, 0.0, 1.0], [8.5, 15.0, 0]) # p: [-1, 16], v: [-15, 15], g: [1,1]
 
     guard = HPolyhedron([
         LazySets.HalfSpace(sparsevec([1], [1.], 3), 0.0), # p <= 0
@@ -53,7 +53,7 @@ function loadBouncingBallShielded()
         LazySets.HalfSpace(sparsevec([1], [1.], 3), 0.0), # p <= 0
         #LazySets.HalfSpace(sparsevec([1], [-1.], 3), 0.0),  # p >= 0
         LazySets.HalfSpace(sparsevec([2], [1.], 3), 0.0),  # v <= 0
-        LazySets.HalfSpace(sparsevec([2], [-1.], 3), 1e-5)  # v <= 0
+        LazySets.HalfSpace(sparsevec([2], [-1.], 3), 1e-5)  # v >= 1e-5
     ])
 
     #constraint = LazySets.API.intersection(constraint, S)
@@ -71,7 +71,53 @@ function loadBouncingBallShielded()
 
     Act = [act1, act2]
 
-    H = EuclideanHybridSystem(S, [constraint], A, edges, Act)
+    H = EuclideanHybridSystem(S, Zonotope(zeros(3), zeros(3, 3)), [constraint], A, edges, Act)
+
+    T = 0.1
+
+    return H, T
+end
+
+function loadBouncingBallShieldedWithInput()
+    A = [0.0 0.0; 1.0 0.0]
+    c = [-9.81, 0.0]
+    u = Zonotope(c, [zero(c)])
+    #u = Zonotope(zeros(2), [zero(c)])
+    S = Hyperrectangle([0.0, 5.5], [10., 5.6]) # p: [-1, 16], v: [-15, 15], g: [1,1]
+
+    guard = HPolyhedron([
+        LazySets.HalfSpace(sparsevec([2], [1.], 2), 0.0), # p <= 0
+        #LazySets.HalfSpace(sparsevec([1], [-1.], 2), 0.0),  # p >= 0
+        LazySets.HalfSpace(sparsevec([1], [1.], 2), 0.0)  # v <= 0
+    ])
+
+    jumpMatrix = [-0.85 0.0; 1.0 0.]
+
+    edges::Vector{Edge} = [Edge(1, guard, jumpMatrix, zeros(2))]
+
+    constraint = HPolyhedron([
+        LazySets.HalfSpace(sparsevec([2], [1.], 2), 0.1), # p <= 0
+        #LazySets.HalfSpace(sparsevec([1], [-1.], 2), 0.0),  # p >= 0
+        LazySets.HalfSpace(sparsevec([1], [1.], 2), 0.0),  # v <= 0
+        LazySets.HalfSpace(sparsevec([1], [-1.], 2), 1e-5)  # v >= 1e-5
+    ])
+
+    #constraint = LazySets.API.intersection(constraint, S)
+
+    act1 = Action(1, HPolyhedron([
+        LazySets.HalfSpace(sparsevec([2], [-1.], 2), -4.0), # p >= 4
+        LazySets.HalfSpace(sparsevec([1], [1.], 2), 0.0),  # v <= 0
+        LazySets.HalfSpace(sparsevec([1], [-1.], 2), 4.0)  # v > -4
+    ]), diagm([0.0, 1.0]), [-4.0, 0.0])
+
+    act2 = Action(2, HPolyhedron([
+        LazySets.HalfSpace(sparsevec([2], [-1.], 2), -4.0), # p >= 4
+        LazySets.HalfSpace(sparsevec([1], [-1.], 2), 0.0)  # v >= 0
+    ]), diagm([0.9, 1.]), [-4.0, 0.0])
+
+    Act = [act1, act2]
+
+    H = EuclideanHybridSystem(S, u, [constraint], A, edges, Act)
 
     T = 0.1
 
